15dedf29ecda13fc7518cd3383dadcabc885081c
es5es5 <es5e5s@nate.com>
2021-01-04 22:00:27 +0900

    Filter List

--- lib/main.dart
+++ lib/main.dart
@@ -30,10 +30,18 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final stores = List<Store>();
   var isLoading = true;
+  var isFilter = false;
 
-  Future fetch() async {
+  @override
+  void initState() {
+    super.initState();
+    getStoreList();
+  }
+
+  getStoreListFilter() async {
     setState(() {
       isLoading = true;
+      isFilter = true;
     });
     var url =
         'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json';
@@ -45,25 +53,55 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       stores.clear();
       jsonStores.forEach((item) {
-        stores.add(Store.fromJson(item));
+        if (isFilter) {
+          if (Store.fromJson(item).remainStat == 'plenty' ||
+              Store.fromJson(item).remainStat == 'some')
+            stores.add(Store.fromJson(item));
+        } else {
+          stores.add(Store.fromJson(item));
+        }
+      });
+      isLoading = false;
+    });
+  }
+
+  Future getStoreList() async {
+    setState(() {
+      isLoading = true;
+      isFilter = false;
+    });
+    var url =
+        'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json';
+    var response = await http.get(url);
+
+    final jsonResult = jsonDecode(response.body);
+    final jsonStores = jsonResult['stores'];
+
+    setState(() {
+      stores.clear();
+      jsonStores.forEach((item) {
+        if (isFilter) {
+          if (Store.fromJson(item).remainStat == 'plenty' ||
+              Store.fromJson(item).remainStat == 'some')
+            stores.add(Store.fromJson(item));
+        } else {
+          stores.add(Store.fromJson(item));
+        }
       });
       isLoading = false;
     });
   }
 
   @override
-  void initState() {
-    super.initState();
-    fetch();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('마스크 재고 있는 곳 : ${stores.length}곳'),
+          title: Text('${isFilter ? '충분히 있는 곳' : '전체'} : ${stores.length}곳'),
           actions: <Widget>[
-            IconButton(icon: Icon(Icons.refresh), onPressed: fetch)
+            IconButton(
+                icon: Icon(Icons.filter_list_outlined),
+                onPressed: getStoreListFilter),
+            IconButton(icon: Icon(Icons.refresh), onPressed: getStoreList),
           ],
         ),
         body: isLoading
@@ -130,8 +168,10 @@ class _MyHomePageState extends State<MyHomePage> {
           remainStat,
           style: TextStyle(color: color, fontWeight: FontWeight.bold),
         ),
-        Text(description,
-        style: TextStyle(color: color),),
+        Text(
+          description,
+          style: TextStyle(color: color),
+        ),
       ],
     );
   }
