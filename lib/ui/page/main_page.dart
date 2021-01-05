8d85c54e0eef04621d30b21fa888b12bef86783f
es5es5 <es5e5s@nate.com>
2021-01-05 22:50:33 +0900

    RemainStatListTile

--- lib/ui/page/main_page.dart
+++ lib/ui/page/main_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_mask/model/store.dart';
+import 'package:flutter_mask/ui/widget/remain_stat_list_tile.dart';
 import 'package:flutter_mask/viewmodel/store_model.dart';
 import 'package:provider/provider.dart';
 
@@ -32,7 +32,7 @@ class MainPage extends StatelessWidget {
             return ListTile(
               title: Text(item.name),
               subtitle: Text(item.addr),
-              trailing: _buildRemainStatWidget(item),
+              trailing: RemainStatListTile(item),
             );
           }).toList(),
         ));
@@ -50,50 +50,4 @@ class MainPage extends StatelessWidget {
     );
   }
 
-  Widget _buildRemainStatWidget(Store store) {
-    var remainStat = '판매중지';
-    var description = '판매중지';
-    var color = Colors.black;
-
-    switch (store.remainStat) {
-      case 'plenty':
-        remainStat = '충분';
-        description = '100개 이상';
-        color = Colors.green;
-        break;
-      case 'some':
-        remainStat = '보통';
-        description = '30개 이상';
-        color = Colors.yellow;
-        break;
-      case 'few':
-        remainStat = '부족';
-        description = '2개 이상';
-        color = Colors.red;
-        break;
-      case 'empty':
-        remainStat = '소진임박';
-        description = '1개 이하';
-        color = Colors.grey;
-        break;
-      default:
-        remainStat = '판매중지';
-        description = '판매중지';
-        color = Colors.black;
-    }
-
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(
-          remainStat,
-          style: TextStyle(color: color, fontWeight: FontWeight.bold),
-        ),
-        Text(
-          description,
-          style: TextStyle(color: color),
-        ),
-      ],
-    );
-  }
 }
---
+++ lib/ui/widget/remain_stat_list_tile.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mask/model/store.dart';
+
+class RemainStatListTile extends StatelessWidget {
+  final Store store;
+
+  RemainStatListTile(this.store);
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildRemainStatWidget(store);
+  }
+
+  Widget _buildRemainStatWidget(Store store) {
+    var remainStat = '판매중지';
+    var description = '판매중지';
+    var color = Colors.black;
+
+    switch (store.remainStat) {
+      case 'plenty':
+        remainStat = '충분';
+        description = '100개 이상';
+        color = Colors.green;
+        break;
+      case 'some':
+        remainStat = '보통';
+        description = '30개 이상';
+        color = Colors.yellow;
+        break;
+      case 'few':
+        remainStat = '부족';
+        description = '2개 이상';
+        color = Colors.red;
+        break;
+      case 'empty':
+        remainStat = '소진임박';
+        description = '1개 이하';
+        color = Colors.grey;
+        break;
+      default:
+        remainStat = '판매중지';
+        description = '판매중지';
+        color = Colors.black;
+    }
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          remainStat,
+          style: TextStyle(color: color, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          description,
+          style: TextStyle(color: color),
+        ),
+      ],
+    );
+  }
+}
